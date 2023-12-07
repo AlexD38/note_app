b6d41fc50d6790a9e447a4ec948677718b85b22f
Alexis Dartiguelongue <alexisdartiguelongue@Oming.local>
2023-12-07 15:35:05 +0100

    fixed login with docs and fetched data in react

--- app/(routes)/users/sign_up/page.tsx
+++ app/(routes)/users/sign_up/page.tsx
@@ -1,26 +1,29 @@
-// "use server";
-// import { sql } from "@vercel/postgres";
-// import { NextResponse } from "next/server";
-// import client from "../../../../database";
+"use server";
+import { sql } from "@vercel/postgres";
+import { NextResponse } from "next/server";
+import client from "../../../../database";
 
-// export default async function InsertUserInfo(userName: any, mail: any, pwd: any) {
-//     try {
-//         const sqlQuery = {
-//             text: `INSERT INTO users (userName, mail, password) VALUES ($1, $2, $3) RETURNING *`,
-//             values: [userName, mail, pwd],
-//         };
-//         const response = await client.query(sqlQuery);
-//         let user = response.rows[0];
-//         if (!user) {
-//             user.isConnected = "false";
-//             return "user not connected";
-//             throw new Error("Failed to fetch user information");
-//         }
-//         user.isConnected = "true";
-//         console.log(user);
-//         // redirect("/dashboard");
-//         return user;
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
+export default async function InsertUserInfo(userName: any, mail: any, pwd: any) {
+    try {
+        if (typeof window === "undefined") {
+            return null;
+        }
+        const sqlQuery = {
+            text: `INSERT INTO users (userName, mail, password) VALUES ($1, $2, $3) RETURNING *`,
+            values: [userName, mail, pwd],
+        };
+        const response = await client.query(sqlQuery);
+        let user = response.rows[0];
+        if (!user) {
+            user.isConnected = "false";
+            return "user not connected";
+            throw new Error("Failed to fetch user information");
+        }
+        user.isConnected = "true";
+        console.log(user);
+        // redirect("/dashboard");
+        return user;
+    } catch (error) {
+        console.log(error);
+    }
+}

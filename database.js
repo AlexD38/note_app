8dd1fa1ea1398e58b09ba7a4c534e3b8cb6c685f
Alexis Dartiguelongue <alexisdartiguelongue@Oming.local>
2023-12-07 12:04:13 +0100

    deleted return into query file

--- database.js
+++ database.js
@@ -1,30 +1,30 @@
 // import du module pg pour la db
-import pg from "pg";
-import dotenv from "dotenv";
-// définition du client
-const Client = pg.Client;
-dotenv.config();
-
-// défintiion de la connection
-const client = new Client({
-    connectionString: process.env.PG_URL,
-});
-
-client.connect(function (err) {
-    if (err) {
-        console.error("Could not connect to PostgreSQL database: ", err);
-    } else {
-        console.log("Connected to PostgreSQL database");
-    }
-});
-
-export default client;
-
 // import pg from "pg";
+// import dotenv from "dotenv";
+// // définition du client
+// const Client = pg.Client;
+// dotenv.config();
 
-// const { Pool } = pg;
-
-// const client = new Pool({
-//     connectionString: process.env.PG_URL + "?sslmode=require",
+// // défintiion de la connection
+// const client = new Client({
+//     connectionString: process.env.PG_URL,
 // });
+
+// client.connect(function (err) {
+//     if (err) {
+//         console.error("Could not connect to PostgreSQL database: ", err);
+//     } else {
+//         console.log("Connected to PostgreSQL database");
+//     }
+// });
+
 // export default client;
+
+import pg from "pg";
+
+const { Pool } = pg;
+
+const client = new Pool({
+    connectionString: process.env.PG_URL + "?sslmode=require",
+});
+export default client;

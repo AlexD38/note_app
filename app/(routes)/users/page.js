9fd8cf4dd68eaea9975468991682a53a1c5d1ec4
Alexis Dartiguelongue <alexisdartiguelongue@Oming.local>
2023-12-07 10:23:08 +0100

    sql from vercel used like in doc

--- app/(routes)/users/page.js
+++ app/(routes)/users/page.js
@@ -1,17 +1,19 @@
 "use server";
 import { redirect } from "next/navigation";
+import { sql } from "@vercel/postgres";
 import client from "../../../database";
 
-export default async function getUserInfo(mail: any, pwd: any) {
+export default async function getUserInfo(mail, pwd) {
     try {
         if (typeof window === "undefined") {
             return null;
         }
-        const sqlQuery = {
-            text: `SELECT * FROM users WHERE mail = $1 AND password = $2;`,
-            values: [mail, pwd],
-        };
-        const response = await client.query(sqlQuery);
+        // const sqlQuery = {
+        //     text: `SELECT * FROM users WHERE mail = $1 AND password = $2;`,
+        //     values: [mail, pwd],
+        // };
+        // const response = await client.query(sqlQuery);
+        const response = await sql`SELECT * FROM users WHERE mail = ${mail} AND password=${pwd};`;
         let user = response.rows[0];
         if (!user) {
             user.isConnected = "false";
